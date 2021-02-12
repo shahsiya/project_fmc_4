1eecea3c48c4a58e70634f5580b6a4145cb60f43
Halid <master_work@outlook.com>
2021-02-13 00:17:19 +0200

    file

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    $('.carousel__inner').slick({
+    $('.carousel').slick({
         speed: 1200,
+        dots: true,
+        dotsClass: "my-dots",
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
         responsive: [{
             breakpoint: 992,
             settings: {
@@ -13,10 +15,25 @@ $(document).ready(function() {
         }]
     });
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $('.carousel2').slick({
+        speed: 1200,
+        dots: false,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left_blue.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right_blue.png"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+
+    $('ul.expert__tabs').on('click', 'li:not(.expert__tab_active)', function() {
         $(this)
-            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+            .addClass('expert__tab_active').siblings().removeClass('expert__tab_active')
+            .closest('div.container').find('div.expert__content').removeClass('expert__content_active').eq($(this).index()).addClass('expert__content_active');
     });
 
     function toggleSlide(item) {
